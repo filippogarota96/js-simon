162057a3a76c9a399f8852d1876ed36f7afa9bcb
Filippo Garota <filogarota101@gmail.com>
2021-03-16 17:42:03 +0100

    Confronto dei numeri e punteggio

--- js/script.js
+++ js/script.js
@@ -12,13 +12,54 @@ function random(min, max) {
 
 var numeri = [];
 
+// i numeri random devono essere sempre diversi
 while (numeri.length < 5) {
   var randomNumber = random(1, 100);
   if (numeri.indexOf(randomNumber) < 0) {
     numeri.push(randomNumber);
   }
 }
+
 var serie = alert(numeri);
 
-
 // All'ok deve partire un timer di 30 secondi
+
+var userNumbers = []
+setTimeout(function() {
+  while (userNumbers.length < 5) {
+   var numeroUtente = parseInt(prompt('Inserisci un numero'));
+    if (userNumbers.includes(numeroUtente)) {
+      alert('Non puoi inserire lo stesso numero');
+    } else {
+      userNumbers.push(numeroUtente);
+    }
+  }
+
+
+   var numeriCorretti = [];
+  for (var i = 0; i < userNumbers.length; i++) {
+    if (numeri.includes(userNumbers[i])) {
+      numeriCorretti.push(userNumbers[i]);
+    }
+  }
+
+  console.log('hai inserito ' + numeriCorretti.length +  ' numeri corretti!');
+  console.log('I numeri che hai ricordato: ' + numeriCorretti);
+}, 30000);
+
+
+
+
+
+
+
+// var userNumbers = []
+// while (numeri.length < 5) {
+//  var numeroUtente = parseInt(prompt('Inserisci un numero'));
+//   if (userNumbers.includes(numeroUtente)) {
+//     alert('Non puoi inserire lo stesso numero');
+//   } else {
+//     userNumbers.push(randomNumber);
+//   }
+// }
+// console.log(userNumbers);
