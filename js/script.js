75ead2538ba5108a5846596d9bf2c572e9e6b1cd
Filippo Garota <filogarota101@gmail.com>
2021-03-16 15:33:16 +0100

    alert con i 5 numeri casuali

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 /*Un alert() espone 5 numeri generati casualmente.
 Da li parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma.*/
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
 
 // creare un alert che genera 5 numeri casuali
 
@@ -21,11 +18,7 @@ while (numeri.length < 5) {
     numeri.push(randomNumber);
   }
 }
-
-for (var i = 0; i < numeri.length; i++) {
-    var serie = numeri[i];
-    alert(serie);
-}
+var serie = alert(numeri);
 
 
 // All'ok deve partire un timer di 30 secondi
